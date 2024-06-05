29215ee3da30a408ac178f4c20521961154b3f2a
Sane-Sunil <sanesunil.falseofficial@gmail.com>
2024-06-05 20:39:39 +0530

    without change

---
+++ main.js
@@ -0,0 +1,13 @@
+let course = document.getElementById("cor");
+
+course.onclick = function() {
+    if (course.innerText === "Course") {
+        course.textContent = "Home";
+        course.href = "#4"; 
+    } else {
+        course.textContent = "Course";
+        course.href = "#"; 
+    }
+}
+// 
+

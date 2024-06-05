1cc1c292815724b503e3a7dd63558854877e337a
Sane-Sunil <sanesunil.falseofficial@gmail.com>
2024-06-05 22:00:59 +0530

    navfix 1.0

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-let course = document.getElementById("cor");
 
-course.onclick = function() {
-    if (course.innerText === "Course") {
-        course.textContent = "Home";
-        course.href = "#4"; 
+let navbarToggler = document.getElementById("navbar-toggler");
+let navbarMenu = document.getElementById("navbar-menu");
+
+navbarToggler.addEventListener("click", function() {
+    if (navbarMenu.style.display === "flex") {
+        navbarMenu.style.display = "none";
+        navbarToggler.classList.remove("open");
     } else {
-        course.textContent = "Course";
-        course.href = "#"; 
+        navbarMenu.style.display = "flex";
+        navbarToggler.classList.add("open");
     }
-}
-// 
-
+});

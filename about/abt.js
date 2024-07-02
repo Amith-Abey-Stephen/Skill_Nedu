f4c5486ad89c3f3c0629f0183e39147cdd2a46da
Amith-Abey-Stephen <amithabeystephen@gmail.com>
2024-07-02 14:51:06 +0530

    add new imgs,about page,minor updates in index..

---
+++ about/abt.js
@@ -0,0 +1,32 @@
+var details = [
+    {
+        "name": "Amenda Maria Johnson",
+        "profile": "",
+        "postion": "CEO"
+    },
+    {
+        "name": "Amith Abey Stephen",
+        "profile": "",
+        "postion": "CTO"
+    },
+    {
+        "name": "Angel K Agnes",
+        "profile": "",
+        "postion": "CCO"
+    },
+    {
+        "name": "Alan Rajan",
+        "profile": "",
+        "postion": "COO"
+    },
+    {
+        "name": "Anasuya S Kurup",
+        "profile": "",
+        "postion": "CMO"
+    },{
+        "name": "Sane Sunil",
+        "profile": "",
+        "postion": "CPO"
+    },
+
+]
